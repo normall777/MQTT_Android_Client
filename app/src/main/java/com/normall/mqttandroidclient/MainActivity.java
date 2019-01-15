0c1f28452a3c47b52776571a9899729da852c9e0
Ilya Izotov <normall777@yandex.ru>
2019-01-15 19:27:16 +0500

    Промежуточный коммит
    Добавлено 2 кнопки на главной активити и 2 поля для ввода адреса сервера
    MQTT добавлен в gradle и в манифест, но пока не реализован

--- app/src/main/java/com/normall/mqttandroidclient/MainActivity.java
+++ app/src/main/java/com/normall/mqttandroidclient/MainActivity.java
@@ -1,15 +1,39 @@
 package com.normall.mqttandroidclient;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
+    //Файл настроек
+    public static final String APP_PREFERENCES = "mysettings";
+    public static final String APP_PREFERENCES_IP_ADD = "ip_add";
+    public static final String APP_PREFERENCES_PORT_MQTT = "mqtt_port";
 
-    private TextView mTextMessage;
+    //Инициализация элементов формы для доступа
+    private EditText editTextIpAdd;
+    private EditText editTextPortMQTT;
+    private SharedPreferences mSettings;
+    private Button buttonConnect;
+    private Button buttonSendTest;
+    private MqttAndroidClient clientMqtt;
+
+
+    //Внутрненние параметры
+    private String ipAdd;
+    private String mqttPort;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +41,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_connect:
+
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+
                     return true;
             }
             return false;
@@ -36,9 +60,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        //Меню
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //Файл настроек
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        //Инициализация эелементов
+        editTextIpAdd = (EditText) findViewById(R.id.editTextIpAdd);
+        editTextPortMQTT = (EditText) findViewById(R.id.editTextPortMQTT);
+        buttonConnect = (Button) findViewById(R.id.buttonConnect);
+        buttonSendTest = (Button) findViewById(R.id.buttonSendTest);
+
+        //Инициализация переменных
+        ipAdd = editTextIpAdd.getText().toString();
+        mqttPort = editTextPortMQTT.getText().toString();
+        
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        buttonSendTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public static byte[] hexStringToByteArray(String s) {
+        byte[] b = new byte[s.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+            int index = i * 2;
+            int v = Integer.parseInt(s.substring(index, index + 2), 16);
+            b[i] = (byte) v;
+        }
+        return b;
+    }
+
+
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        ipAdd = editTextIpAdd.getText().toString();
+        mqttPort = editTextPortMQTT.getText().toString();
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putString(APP_PREFERENCES_IP_ADD, ipAdd);
+        editor.putString(APP_PREFERENCES_PORT_MQTT, mqttPort);
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        if (mSettings.contains(APP_PREFERENCES_IP_ADD)){
+            ipAdd = mSettings.getString(APP_PREFERENCES_IP_ADD, getString(R.string.default_ip_add));
+            editTextIpAdd.setText(ipAdd);
+        }
+
+        if (mSettings.contains(APP_PREFERENCES_PORT_MQTT)) {
+            mqttPort = mSettings.getString(APP_PREFERENCES_PORT_MQTT, getString(R.string.default_mqtt_port));
+            editTextPortMQTT.setText(mqttPort);
+        }
     }
 
 }
